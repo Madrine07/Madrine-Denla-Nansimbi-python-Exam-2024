1cbfa07962e210590e84ca92eca28f9b33f43e6c
letty23244 <denladunamis@gmail.com>
2024-12-12 11:36:29 +0300

    FINAL EXAM

--- section_b/question_two.py
+++ section_b/question_two.py
@@ -1,15 +1,16 @@
+def student_details():
+     
+  student_name = input("Enter student name: ")
+  student_number = input("Enter the student number: ")
+  programming = int(input("Enter the programming mark: "))
+  data_science = int(input("Enter the data science mark: "))
+  computer_applications = int(input("Enter the computer applications mark: "))
+  total_marks = programming + data_science + computer_applications
+  total_subjects = 3
+  average_mark = total_marks / total_subjects
 
-student_name = input("Enter student name: ")
-student_number = input("Enter the student number: ")
-programming = int(input("Enter the programming mark: "))
-data_science = int(input("Enter the data science mark: "))
-computer_applications = int(input("Enter the computer applications mark: "))
-total_marks = programming + data_science + computer_applications
-total_subjects = 3
-average_mark = total_marks / total_subjects
-
-print(f"The average mark of {student_name} with a student number of {student_number} is: {average_mark:.3f}")
-
+  print(f"The average mark of {student_name} with a student number of {student_number} is: {average_mark:.3f}")
+student_details()
 
 # 2) ii)
 def car_miles_per_gallon_used():
